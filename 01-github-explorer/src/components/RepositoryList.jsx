a0e33377e1c86cd783f95865ed14ca72ea0f3f40
Wellington R <sousa_wellington@outlook.com>
2021-03-28 13:49:00 -0300

    🔧feat: first conponents

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+          <a href="http://">Acessar repositorio</a>
+        </li>
+        
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+          <a href="http://">Acessar repositorio</a>
+        </li>
+        
+        <li>
+          <strong>unform</strong>
+          <p>Form in React</p>
+          <a href="http://">Acessar repositorio</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
