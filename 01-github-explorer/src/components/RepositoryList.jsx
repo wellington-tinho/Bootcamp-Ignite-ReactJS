f39d7b0837bc4109bde9d0f5aa88c7ac910686ab
Wellington R <sousa_wellington@outlook.com>
2021-03-28 14:19:54 -0300

    🔧feat: React properties

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,25 +1,22 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "Ignite",
+  description: "Forms in React",
+  link: "https://github.com/wrtinho"
+}
+
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="http://">Acessar repositorio</a>
-        </li>
-        
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="http://">Acessar repositorio</a>
-        </li>
-        
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="http://">Acessar repositorio</a>
-        </li>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+
       </ul>
     </section>
   );
