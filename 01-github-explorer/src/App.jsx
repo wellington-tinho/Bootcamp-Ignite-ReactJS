e52192c09f5caaf12824143d0b134763f2d437e9
Wellington R <sousa_wellington@outlook.com>
2021-03-27 21:04:10 -0300

    🔧 feat:  structure react

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,3 @@
-export function App(){
-  return <h1>hello Ignite</h1>
+export function App() {
+  return <h1>Hello World</h1>
 }
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode: 'development', // Aponta para o Webpack qual o modo deverá ser utilizado para compilar os arquivos
-  entry: path.resolve(__dirname, 'src', 'index.jsx'), // Arquivo de entrada que o webpack irá buscar
-  output: {
-    path: path.resolve(__dirname, 'dist'), // Path da pasta destino com o código convertido pelo webpack
-    filename: 'bundle.js' // Nome do arquivo destino com o código convertido pelo webpack
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output:{
+    path: path.resolve(__dirname,'dist'),
+    filename:'bundle.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Extensões que o webpack irá aceitar (necessário pois estamos utilizando arquivos .jsx)
+  resolve:{
+    extensions:['.js','.jsx'],
   },
-  module: { // Configurações para dizer ao webpack como se comportar para cada tipo de arquivo (.js, .png, etc)
-    rules: [ // Array de objetos contendo as regras para cada module
+  module:{
+    rules:[
       {
-        test: /\.jsx$/, // Recebe um RegEx para testar se o arquivo é da extensão que queremos
-        exclude: /node_modules/, // Arquivos que serão ignorados na compilação desta regra
-        use: 'babel-loader' // Loader do Babel para compilar os arquivos JavaScript
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       }
     ],
   }
-}
+}; 
