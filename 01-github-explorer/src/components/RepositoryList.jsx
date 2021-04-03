a4c29499ced9bc33c9e48baf90a02b8e03e53615
Wellington R <sousa_wellington@outlook.com>
2021-04-03 08:35:33 -0300

    ⭐feat: using useEffect

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+
+//
 
 const repository = {
   name: "Ignite",
@@ -8,6 +12,12 @@ const repository = {
 
 
 export function RepositoryList() {
+  const [repositories,setRepositories] = useState([])
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users/wrtinho/repos').then(response => response.json()).then(data=>setRepositories(data))
+  },[]);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
