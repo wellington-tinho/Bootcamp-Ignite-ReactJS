43688a6624790021054435d904f1fe78ba70bdd3
Wellington R <sousa_wellington@outlook.com>
2021-04-03 11:03:18 -0300

    ⭐feat: components with TypeScript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 
-
+interface Repository{
+  name: string;
+  description: string;
+  html_url: string;
+}
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/users/wrtinho/repos').then(response => response.json()).then(data => setRepositories(data))
