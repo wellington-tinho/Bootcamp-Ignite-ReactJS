ffe60867d312eeac04a4539d2253700189456635
Wellington R <sousa_wellington@outlook.com>
2021-08-13 09:46:31 -0300

    ✔ add: Formatting values

--- 02-dtmoney/src/components/TransationsTable/index.tsx
+++ 02-dtmoney/src/components/TransationsTable/index.tsx
@@ -33,9 +33,19 @@ export function TransationTable(){
           {transactions.map(transaction => (
               <tr key={transaction.id}>
                 <td> {transaction.title} </td>
-                <td className={transaction.type}>R${transaction.amount}</td>
+                <td className={transaction.type}> {new Intl.NumberFormat('pt-BR',{
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+                </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>
+                  { 
+                    new Intl.DateTimeFormat('pt-BR').format( 
+                      new Date(transaction.createdAt) 
+                    ) 
+                  }
+                </td>
               </tr>   
             )
           )}
